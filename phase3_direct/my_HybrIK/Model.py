05585f85b6ceaffb40c37860de978464e142ac16
RHnejad <r.h817@yahoo.com>
2023-05-01 14:45:01 +0200

    adding phase3

---
+++ phase3_direct/my_HybrIK/Model.py
@@ -0,0 +1,146 @@
+import torch 
+import torch.nn as nn
+from torch.nn import functional as F
+
+from Resnet import ResNet
+from utils import visualize_3d,plot_heat_map
+#https://github.dev/Jeff-sjtu/HybrIK/tree/b8cfeeb7df2d7c8024865751cc17664a8272557b/hybrik/models
+
+
+class Model_3D(nn.Module):
+    def __init__(self):
+        super(Model_3D, self).__init__()
+        
+        #_________
+        self.deconv_dim = [256,256,256] # kwargs['NUM_DECONV_FILTERS']
+        self.num_joints = 17 #kwargs['NUM_JOINTS']
+        self.depth_dim = 64 #kwargs['EXTRA']['DEPTH_DIM']
+        self.norm_type = 'softmax'
+        self.height_dim = 64
+        self.width_dim = 64
+        
+        self._norm_layer = nn.BatchNorm2d
+        
+        #__________preact___________
+        self.preact = ResNet("resnet50") 
+        
+        # Imagenet pretrain model
+        import torchvision.models as tm   
+        x = tm.resnet50(pretrained=True)
+        self.feature_channel = 2048
+        
+        model_state = self.preact.state_dict()
+        state = {k: v for k, v in x.state_dict().items()
+                 if k in self.preact.state_dict() and v.size() == self.preact.state_dict()[k].size()}
+        model_state.update(state)
+        self.preact.load_state_dict(model_state)
+           
+        #________deconv_layers______
+        self.deconv_layers = self._make_deconv_layer()
+        
+        #________final_layer________
+        self.final_layer = nn.Conv2d(
+            self.deconv_dim[2], self.num_joints * self.depth_dim, kernel_size=1, stride=1, padding=0)
+    
+    
+    def _make_deconv_layer(self):
+        deconv_layers = []
+        deconv1 = nn.ConvTranspose2d(
+            self.feature_channel, self.deconv_dim[0], kernel_size=4, stride=2, padding=int(4 / 2) - 1, bias=False)
+        bn1 = self._norm_layer(self.deconv_dim[0])
+        deconv2 = nn.ConvTranspose2d(
+            self.deconv_dim[0], self.deconv_dim[1], kernel_size=4, stride=2, padding=int(4 / 2) - 1, bias=False)
+        bn2 = self._norm_layer(self.deconv_dim[1])
+        deconv3 = nn.ConvTranspose2d(
+            self.deconv_dim[1], self.deconv_dim[2], kernel_size=4, stride=2, padding=int(4 / 2) - 1, bias=False)
+        bn3 = self._norm_layer(self.deconv_dim[2])
+
+        deconv_layers.append(deconv1)
+        deconv_layers.append(bn1)
+        deconv_layers.append(nn.ReLU(inplace=True))
+        deconv_layers.append(deconv2)
+        deconv_layers.append(bn2)
+        deconv_layers.append(nn.ReLU(inplace=True))
+        deconv_layers.append(deconv3)
+        deconv_layers.append(bn3)
+        deconv_layers.append(nn.ReLU(inplace=True))
+
+        return nn.Sequential(*deconv_layers)
+    
+
+    def norm_heatmap(self, norm_type, heatmap ):
+        # Input tensor shape: [N,C,...]
+        shape = heatmap.shape
+        if norm_type == 'softmax':
+            heatmap = heatmap.reshape(*shape[:2], -1)
+            # global soft max
+            heatmap = F.softmax(heatmap, 2)
+            return heatmap.reshape(*shape)
+        else:
+            raise NotImplementedError
+            
+    def forward(self, x):
+    
+        batch_size = x.shape[0]
+    
+        x0 = self.preact(x)
+        out = self.deconv_layers(x0)
+        out = self.final_layer(out)
+        
+        print(out.shape)
+         
+        out = out.reshape((out.shape[0], self.num_joints, -1))
+        out = self.norm_heatmap(self.norm_type, out)
+        assert out.dim() == 3, out.shape
+        
+        maxvals = torch.ones((*out.shape[:2], 1), dtype=torch.float, device=out.device) #check
+
+        heatmaps = out / out.sum(dim=2, keepdim=True) #out.sun is 1 if softmax so not actually needed
+        
+        heatmaps = heatmaps.reshape((heatmaps.shape[0], self.num_joints, self.depth_dim, self.height_dim, self.width_dim))
+        
+        hm_x = heatmaps.sum((2, 3))
+        hm_y = heatmaps.sum((2, 4))
+        hm_z = heatmaps.sum((3, 4))
+        
+        
+        print("+++++++",hm_z.shape)
+        
+        if torch.cuda.is_available():
+            hm_x = hm_x * torch.cuda.comm.broadcast(torch.arange(hm_x.shape[-1]).type(
+                torch.cuda.FloatTensor), devices=[hm_x.device.index])[0]
+            hm_y = hm_y * torch.cuda.comm.broadcast(torch.arange(hm_y.shape[-1]).type(
+                torch.cuda.FloatTensor), devices=[hm_y.device.index])[0]
+            hm_z = hm_z * torch.cuda.comm.broadcast(torch.arange(hm_z.shape[-1]).type(
+                torch.cuda.FloatTensor), devices=[hm_z.device.index])[0]
+        else :
+            hm_x = hm_x * torch.arange(hm_x.shape[-1]).type(torch.FloatTensor)
+            hm_y = hm_y * torch.arange(hm_y.shape[-1]).type(torch.FloatTensor)
+            hm_z = hm_z * torch.arange(hm_z.shape[-1]).type(torch.FloatTensor)
+            
+        print(hm_z.shape,hm_z )    
+            
+        coord_x = hm_x.sum(dim=2, keepdim=True)
+        coord_y = hm_y.sum(dim=2, keepdim=True)
+        coord_z = hm_z.sum(dim=2, keepdim=True)
+
+        coord_x = coord_x / float(self.width_dim) #- 0.5
+        coord_y = coord_y / float(self.height_dim) #- 0.5
+        coord_z = coord_z / float(self.depth_dim) #- 0.5
+        
+        #  -0.5 ~ 0.5
+        pred_uvd_jts_29 = torch.cat((coord_x, coord_y, coord_z), dim=2)
+
+        pred_uvd_jts_29_flat = pred_uvd_jts_29.reshape((batch_size, self.num_joints * 3)) 
+        
+        # print("****",coord_x, coord_y, coord_z)
+        combined_tensor = torch.cat((coord_x, coord_y, coord_z), dim=-1)
+        numpy_array = combined_tensor.detach().cpu().numpy().reshape(17, 3)
+        # print("****",coord_x.shape, numpy_array)
+
+        # visualize_3d(numpy_array.copy(), numpy_array)
+        
+        return out
+    
+    
+    
---
+++ phase3_direct/my_HybrIK/test.py
@@ -0,0 +1,63 @@
+from Model import Model_3D
+import numpy as np
+import torch
+import cv2
+from torch.utils.data import DataLoader
+from H36_dataset import *
+
+def generate_image():
+    img_height = 256 #224
+    img_width = 256 #224
+    num_channels = 3
+
+    # # Generate a random image with values between 0 and 255
+    # random_image = np.random.randint(0, 256, size=(img_height, img_width, num_channels), dtype=np.uint8)
+
+    # # Convert the NumPy array to a PyTorch tensor
+    # random_image_tensor = torch.from_numpy(random_image.transpose((2, 0, 1))).float()
+
+    # # Normalize the tensor values to be between 0 and 1
+    # random_image_tensor /= 255.0
+    
+    
+    img = cv2.imread("./please.png")
+
+    # Resize image to 256x256 pixels
+    img_resized = cv2.resize(img, (img_width, img_height))
+
+    # Convert image to a PyTorch tensor with float values between 0 and 1
+    img_tensor = torch.from_numpy(img_resized.transpose((2, 0, 1))).float() / 255.0
+
+
+    # Add a batch dimension to the tensor
+    random_image_tensor = img_tensor.unsqueeze(0)
+
+
+    # cv2.imshow('Resized Image', img_resized)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+        
+    return random_image_tensor
+
+
+if __name__ == "__main__":
+    
+    training_set = H36_dataset(num_cams=num_cameras, subjectp=subjects[0:1], is_train = True) 
+    test_set     = H36_dataset(num_cams=num_cameras, subjectp=subjects[0:1] , is_train = False)
+    
+    batch_size=64
+
+    train_loader = DataLoader( training_set, shuffle=True, batch_size=batch_size, num_workers= 1)
+    test_loader = DataLoader(test_set, shuffle=True, batch_size=batch_size, num_workers=1)
+    
+    x = generate_image()
+    
+    d2d, d3d, frame = training_set.__getitem__(300)
+
+    model= Model_3D()
+     
+    y= model(x)
+    y2 = model(frame)
+    
+    print(x.shape,y.shape)
+    print(frame.shape,y2.shape)
